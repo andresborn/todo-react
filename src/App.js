ca241f95dc5a6e49f38b764eff06288c19f7be6b
andresborn <andres.born@gmail.com>
2021-02-11 17:49:27 -0600

    Added close btn on hover

--- src/App.js
+++ src/App.js
@@ -1,31 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 
 const App = () => {
-
   const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
+  const [taskList, setTaskList] = useState([]);
+  const [hovered, setHovered] = useState(false);
 
-  
+  const onChange = (e) => {
+    const id = nanoid();
+    setTask({
+      value: e.target.value,
+      id: id,
+    });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, task]);
+    console.log(task);
+    setTask({ value: "" });
+  };
+
+  const toggleHover = () => setHovered(!hovered);
+
+  const del = (e) => {
+    console.log(e.target.id);
+  };
+
+  const RenderList = () => {
+    return taskList.map((task, i) => {
+      return (
+        <div
+          onMouseEnter={toggleHover}
+          onMouseLeave={toggleHover}
+          key={i}
+          data-attribute={i}
+          className="breadcrumb shadow-sm justify-content-between  tarea"
+        >
+          <div className="col-10">
+            <p>{task.value}</p>
+          </div>
+
+          <button
+            type="button"
+            className={`close col-2 ${hovered ? "d-flex" : "hidden"}`}
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      );
+    });
+  };
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 text-center">
-          <h1 className="ewert">Tareas de vaquero</h1>
+          <h1 className="ewert">Tareas Vaqueras</h1>
         </div>
       </div>
       <div className="row">
         <div className="col-12">
-          <form className="form-group">
-            <input type="text" className="col-12 form-control eczar" placeholder="¿Qué hay para hoy?"/>
+          <form onSubmit={onSubmit} className="form-group">
+            <input
+              onChange={onChange}
+              type="text"
+              className="col-12 form-control eczar"
+              placeholder="¿Qué hay para hoy?"
+              value={task.value}
+              required
+            />
           </form>
         </div>
       </div>
       <div className="row eczar">
         <div className="col">
-          <p className="breadcrumb shadow-sm tarea ">Tarea 1</p>
-          <p className="breadcrumb shadow-sm tarea ">Tarea 2</p>
+          <RenderList />
         </div>
       </div>
     </div>
