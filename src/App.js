ece5b64271c8e8c9b7438510a9cc25da16ab3519
andresborn <andres.born@gmail.com>
2021-02-15 15:00:14 -0600

    Added backend. GET and PUT tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import List from "./components/List";
@@ -8,36 +8,87 @@ const App = () => {
   const [task, setTask] = useState("");
   const [taskList, setTaskList] = useState([]);
 
+  useEffect(() => {
+    const getTaskListFromServer = async () => {
+      const serverTaskList = await getData()
+      setTaskList(serverTaskList)
+    }
+    getTaskListFromServer()
+  }, [])
+
+  // Fetch Task List
+  const getData = async () => {
+    const res = await fetch("https://assets.breatheco.de/apis/fake/todos/user/andresborn")
+    const data = await res.json()
+    return data
+  }
+
+  // Set Task List on Server
+  const setTaskOnServer = async (taskList) => {
+    await fetch("https://assets.breatheco.de/apis/fake/todos/user/andresborn",
+    {
+      method:'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(taskList)
+    })
+  }
+
+  // Controlled form
   const onChange = (e) => {
     const id = nanoid();
     setTask({
       value: e.target.value,
       id: id,
       important: false,
-      hover: false
+      hover: false,
+      label: e.target.value,
+      done: false
     });
   };
+
+  // Add Task to Task List
   const onSubmit = (e) => {
     e.preventDefault();
-    setTaskList([...taskList, task]);
-    setTask({ value: "" });
+    taskList.push(task)
+    setTaskOnServer(taskList)
+    setTaskList([...taskList])
+
+    // Note:
+    // Initially we were trying to do:
+    // setTaskList([...taskList, task]) and then
+    // setTaskOnServer(taskList)
+    // but, setTaskList is async, so our updated taskList isn't ready to be sent.
+
+    // How we've fixed this is:
+    // taskList.push(task) inmediately updates our taskList, then
+    // setTaskOnServer(taskList) updates our server, and finally
+    // setTaskList([...taskList]) updates our UI
+    
+    setTask({ value: "" }); // Reset task form
   };
 
+  // Delete Task
   const deleteTask = (id) => {
     const filteredList = taskList.filter((task) => !(task.id === id))
+    setTaskOnServer(filteredList)
     setTaskList(filteredList)
+
   }
 
+  // DoubleClick to toggle important task
   const toggleImportant = (id) => {
-    setTaskList(taskList.map(task => {
+    const importantList = taskList.map(task => {
       if (task.id === id) {
         task.important = !task.important
         return task
       } else {
         return task
       }
-    }))
+    })
+    setTaskOnServer(importantList)
+    setTaskList(importantList)
   }
+  // Activate hover to conditionally display delete button
   const onHoverEnter = (id) => {
     setTaskList(taskList.map(task => {
       if (task.id === id) {
@@ -48,6 +99,7 @@ const App = () => {
       }
     }))
   }
+
   const onHoverLeave = (id) => {
     setTaskList(taskList.map(task => {
       if (task.id === id) {
